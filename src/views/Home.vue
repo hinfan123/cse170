ee00ec63a34fb5c75d6f21aee568f0562483c179
Kass Chupongstimun <k_724@live.com>
2018-10-29 20:01:00 -0700

    init VueJS

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+	<div class="grid-container">
+		<div class="grid">
+			<div class="recipe-box"></div>
+			<div class="recipe-box"></div>
+			<div class="recipe-box"></div>
+			<div class="recipe-box"></div>
+			<div class="recipe-box"></div>
+			<div class="recipe-box"></div>
+			<div class="recipe-box"></div>
+			<div class="recipe-box"></div>
+		</div>
+	</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
