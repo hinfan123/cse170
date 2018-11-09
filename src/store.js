3651209d02a53d54b2a340ed34378b516235d4f6
Ally Landingin <al.landingin@gmail.com>
2018-11-08 18:50:32 -0800

    Recipe List
    Updated gif URLs

--- src/store.js
+++ src/store.js
@@ -85,7 +85,7 @@ export default new Vuex.Store({
 					{ // step 1
 						n: 1,
 						title: 'Make the Sauce',
-						gifURL: 'https://gph.is/2ATry3P',
+						gifURL: 'https://media.giphy.com/media/64aHH8NS43uqnJT9q0/giphy.gif',
 						details: 'Add chicken broth, oyster sauce, fish sauce, white sugar, and brown sugar together in bowl. Whisk together until well blended',
 						timer: true,
 						duration: 0.5
@@ -93,7 +93,7 @@ export default new Vuex.Store({
 					{ // step 2
 						n: 2,
 						title: 'Prepare Basil',
-						gifURL: 'https://gph.is/2ASazyY',
+						gifURL: 'https://media.giphy.com/media/2wXXmfG8w5Dpzcf6Fz/giphy.gif',
 						details: 'Thinly slice basil leaves',
 						timer: false,
 						duration: 0
@@ -101,7 +101,7 @@ export default new Vuex.Store({
 					{ // step 3
 						n: 3,
 						title: 'Cut Shallots',
-						gifURL: 'https://gph.is/2zDJAoY',
+						gifURL: 'https://media.giphy.com/media/LpkyFm03sUdiEAxZAy/giphy.gif',
 						details: 'Peel and thinly cut shallots',
 						timer: false,
 						duration: 0
@@ -109,7 +109,7 @@ export default new Vuex.Store({
 					{ // step 4
 						n: 4,
 						title: 'Mince Garlic',
-						gifURL: 'https://gph.is/2zAGmT7',
+						gifURL: 'https://media.giphy.com/media/9P3DEEByoWpOnuDfIv/giphy.gif',
 						details: 'Peel, slice, and mince garlic',
 						timer: false,
 						duration: 0
@@ -117,15 +117,15 @@ export default new Vuex.Store({
 					{ // step 5
 						n: 5,
 						title: 'Cut Peppers',
-						gifURL: 'https://gph.is/2Ph2psO',
-						details: 'Thinly cut garlic.',
+						gifURL: 'https://media.giphy.com/media/82wZfAVtQnVgXNzgZa/giphy.gif',
+						details: 'Thinly cut peppers.',
 						timer: false,
 						duration: 0
 					},
 					{ // step 6
 						n: 6,
 						title: 'Chop Chicken',
-						gifURL: 'https://gph.is/2zCJTjX',
+						gifURL: 'https://media.giphy.com/media/XJLZlF6t9cHZXH7i98/giphy.gif',
 						details: 'Coarsely chop chicken until it meets a grounded texture.',
 						timer: true,
 						duration: 240
@@ -133,7 +133,7 @@ export default new Vuex.Store({
 					{ // step 7
 						n: 7,
 						title: 'Stirfry Chicken',
-						gifURL: 'https://gph.is/2RIDCu7',
+						gifURL: 'https://media.giphy.com/media/3gJQeAEXs9yoslqNqp/giphy.gif',
 						details: 'Add oil to pan. Add grounded chicken to pan and stirfry.',
 						timer: true,
 						duration: 120
@@ -141,7 +141,7 @@ export default new Vuex.Store({
 					{ // step 8
 						n: 8,
 						title: 'Add chopped ingredients',
-						gifURL: 'https://gph.is/2RIDCu7',
+						gifURL: 'https://media.giphy.com/media/9RZXjV2h5LdPaNtsti/giphy.gif',
 						details: 'Stir in shallots, garlic, and sliced chillies.',
 						timer: true,
 						duration: 120
@@ -149,7 +149,7 @@ export default new Vuex.Store({
 					{ // step 9
 						n: 9,
 						title: 'Add a bit of sauce',
-						gifURL: 'https://gph.is/2ASfdgn',
+						gifURL: 'https://media.giphy.com/media/QmIEXxHmxa8mRZzvuw/giphy.gif',
 						details: 'Add only a splash of the sauce mixture. Keep stirring until the sauce is absorbed in the chicken.',
 						timer: true,
 						duration: 120
@@ -157,7 +157,7 @@ export default new Vuex.Store({
 					{ // step 10
 						n: 10,
 						title: 'Add the rest of sauce',
-						gifURL: 'https://gph.is/2ATITK6',
+						gifURL: 'https://media.giphy.com/media/2tPowUFfm7AIVFRcSU/giphy.gif',
 						details: 'Add the rest of the sauce mixture. Keep stirring until the sauce is absorbed in the chicken.',
 						timer: true,
 						duration: 180
@@ -165,7 +165,7 @@ export default new Vuex.Store({
 					{ // step 11
 						n: 11,
 						title: 'Add Basil',
-						gifURL: 'https://gph.is/2PeiwqV',
+						gifURL: 'https://media.giphy.com/media/5tvKUuaMkJz8pifThh/giphy.gif',
 						details: 'Add the basil and stir into the chicken.',
 						timer: true,
 						duration: 120
@@ -259,7 +259,7 @@ export default new Vuex.Store({
 					{ // step 1
 						n: 1,
 						title: 'Boil Tomato Sauce',
-						gifURL: 'https://gph.is/2RP6Jwf',
+						gifURL: 'https://media.giphy.com/media/1inOgeWV3EcUbfy06h/giphy.gif',
 						details: 'Using a tomato sauce of your choice, stir and bring it to a simmer on medium heat. Then reduce heat to low to keep sauce warm.',
 						timer: true,
 						duration: 180
@@ -267,7 +267,7 @@ export default new Vuex.Store({
 					{ // step 2
 						n: 2,
 						title: 'Pre-Heat Oven',
-						gifURL: 'https://gph.is/2ATihsE',
+						gifURL: 'https://media.giphy.com/media/LXQKgwV69cmvIvSNCa/giphy.gif',
 						details: 'Pre-heat oven to 500 F and keep on while preparing meatballs',
 						timer: false,
 						duration: 0
@@ -275,7 +275,7 @@ export default new Vuex.Store({
 					{ // step 3
 						n: 3,
 						title: 'Make Slurry',
-						gifURL: 'https://gph.is/2RLLJ9B',
+						gifURL: 'https://media.giphy.com/media/YjuF7BNn8liWKivFXs/giphy.gif',
 						details: 'Add breadcrumbs, milk, egg, Italian seasoning, olive oil, garlic powder, and red flakes together into a bowl. Whisk together until you have a thick, slurry consistency',
 						timer: true,
 						duration: 60
@@ -283,7 +283,7 @@ export default new Vuex.Store({
 					{ // step 4
 						n: 4,
 						title: 'Mix Meat',
-						gifURL: 'https://gph.is/2AV18Pn',
+						gifURL: 'https://media.giphy.com/media/3d87Q5fPxDUSyreVgL/giphy.gif',
 						details: 'Combine beef and veal in a large bowl. Add salt, black pepper, white pepper, Parmigiano-Reggiano, and slurry made in previous step. Mix until combined',
 						timer: true,
 						duration: 180
@@ -291,7 +291,7 @@ export default new Vuex.Store({
 					{ // step 5
 						n: 5,
 						title: 'Prepare baking sheet',
-						gifURL: 'https://gph.is/2RLQIah',
+						gifURL: 'https://media.giphy.com/media/kPmlOPx0BzX7kfyxOQ/giphy.gif',
 						details: 'Line baking sheet with aluminum foil. Brush it lightly with oil.',
 						timer: false,
 						duration: 0
@@ -299,7 +299,7 @@ export default new Vuex.Store({
 					{ // step 5
 						n: 5,
 						title: 'Scoop Meatballs',
-						gifURL: 'https://gph.is/2zDQuuf',
+						gifURL: 'https://media.giphy.com/media/tJdwJbehOyPPEqQcQZ/giphy.gif',
 						details: 'Using a sorbet scoop, scoop meat mixture and place on baking sheet. Finish out the rest of the mixture.' ,
 						timer: false,
 						duration: 0
@@ -307,7 +307,7 @@ export default new Vuex.Store({
 					{ // step 5
 						n: 5,
 						title: 'Broil Meatballs',
-						gifURL: 'https://gph.is/2ATYg5a',
+						gifURL: 'https://media.giphy.com/media/cRLPrORIDoY1NcTQKh/giphy.gif',
 						details: 'Place meatballs into the oven and broil until brown.',
 						timer: true,
 						duration: 300
@@ -315,7 +315,7 @@ export default new Vuex.Store({
 					{ // step 6
 						n: 6,
 						title: 'Transfer Meatballs',
-						gifURL: 'https://gph.is/2ATYg5a',
+						gifURL: 'https://media.giphy.com/media/9GJcz3aUXyDWv0eN1I/giphy.gif',
 						details: 'Transfer meatballs into the tomato sauce. Turn up heat back to simmer to further cook the meatballs into the tomato sauce.',
 						timer: true,
 						duration: 180
@@ -379,7 +379,7 @@ export default new Vuex.Store({
 					{ // step 1
 						n: 1,
 						title: 'Cut Peppers',
-						gifURL: 'https://gph.is/2Ph2psO',
+						gifURL: 'https://media.giphy.com/media/82wZfAVtQnVgXNzgZa/giphy.gif',
 						details: 'Thinly cut up Serrano peppers.',
 						timer: false,
 						duration: 0
@@ -387,7 +387,7 @@ export default new Vuex.Store({
 					{ // step 2
 						n: 2,
 						title: 'Cut Cilantro',
-						gifURL: 'https://gph.is/2RHxyCk',
+						gifURL: 'https://media.giphy.com/media/9xrbFyFTwl3XfJZQwb/giphy.gif',
 						details: 'Chop cilantro to desired thickness',
 						timer: false,
 						duration: 0
@@ -395,7 +395,7 @@ export default new Vuex.Store({
 					{ // step 3
 						n: 3,
 						title: 'Chop Onions',
-						gifURL: 'https://gph.is/2RHhvV2',
+						gifURL: 'https://media.giphy.com/media/1itBKqHdcuc0LihqzH/giphy.gif',
 						details: 'Chop the onion using technique showng',
 						timer: false,
 						duration: 0
@@ -403,7 +403,7 @@ export default new Vuex.Store({
 					{ // step 4
 						n: 4,
 						title: 'Quick chop',
-						gifURL: 'https://gph.is/2RGDT0M',
+						gifURL: 'https://media.giphy.com/media/5n4corltzrkahqtP9u/giphy.gif',
 						details: 'Add onions, cilantro, and serrano peppers together and give it a slight chop.',
 						timer: true,
 						duration: 30
@@ -411,37 +411,29 @@ export default new Vuex.Store({
 					{ // step 5
 						n: 5,
 						title: 'Grinding Technique',
-						gifURL: 'https://gph.is/2ATO0tr',
+						gifURL: 'https://media.giphy.com/media/69C1Ee1LEWGvBddcxt/giphy.gif',
 						details: 'Add in kosher salt and use grinding technique to create a thinner, paste-like consistency.',
 						timer: true,
 						duration: 120
 					},
 					{ // step 6
 						n: 6,
-						title: 'Grinding Technique',
-						gifURL: 'https://gph.is/2ATO0tr',
-						details: 'Add in kosher salt and use grinding technique to create a thinner, paste-like consistency.',
-						timer: true,
-						duration: 120
+						title: 'Prepare Avocados',
+						gifURL: 'https://media.giphy.com/media/fH0bH5jZVmNBsiDPR2/giphy.gif',
+						details: 'Cut and remove pit of avocados.',
+						timer: false,
+						duration: 0
 					},
 					{ // step 7
 						n: 7,
-						title: 'Prepare Avocados',
-						gifURL: 'https://gph.is/2RGixkf',
-						details: 'Cut and remove pit of avocados.',
+						title: 'Mash Guacamole',
+						gifURL: 'https://media.giphy.com/media/vcdiV9CpApFB8JX0GS/giphy.gif',
+						details: 'Add in cilantro and onion paste. Add extra cilantro and lime for taste. Mash everything to desired consistency.',
 						timer: false,
 						duration: 0
 					},
 					{ // step 8
 						n: 8,
-						title: 'Mash Guacamole',
-						gifURL: 'https://gph.is/2Ph0gxh',
-						details: 'Add in cilantro and onion paste. Add extra cilantro and lime for taste. Mash everything to desired consistency.',
-						timer: false,
-						duration: 0
-					},
-					{ // step 9
-						n: 9,
 						title: 'Finished!',
 						gifURL: 'http://i68.tinypic.com/adzu6e.png',
 						details: 'Enjoy your delicious guacamole with some chips!',
